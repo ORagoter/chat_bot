911af755d86efedd4ea6127d7487f0a323c2e53e
ORagoter <offnikolai.mokrushin@gmail.com>
2024-05-15 14:03:35 +0500

    venv

--- bot/app.py
+++ bot/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
 
+from processing_question import processing_questions
+
 app = Flask(__name__)
 
 @app.route('/bot', methods=['POST'])
 def handle_data():
     data = request.json
     # Обработка полученных данных
-    processed_data = data['data']  # Пример обработки
+    processed_data = processing_questions(data['data'])
     # Возвращаем ответ
     return jsonify({'result': processed_data})
 

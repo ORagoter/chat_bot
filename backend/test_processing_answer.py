5b397aa3704bb8632a06e78ae5aeb91f3de8e087
ORagoter <offnikolai.mokrushin@gmail.com>
2024-05-30 04:18:46 +0500

    new_model + refact DB

--- backend/test_processing_answer.py
+++ backend/test_processing_answer.py
@@ -32,3 +32,48 @@ def preprocess_text(text):
     preprocessed_text = ' '.join(lemmatized_tokens)
     
     return preprocessed_text
+
+
+
+
+import psycopg2
+
+# Устанавливаем параметры подключения
+conn_params = {
+    'dbname': 'postgres',
+    'user': 'postgres.zzyahwklsrihlglqbsfd',
+    'password': 'xy9$G/Cy~b~)&+_',
+    'host': 'aws-0-eu-central-1.pooler.supabase.com',
+    'port': '5432'
+}
+
+# Устанавливаем соединение
+conn = psycopg2.connect(**conn_params)
+
+# Создаем курсор для выполнения запросов
+cur = conn.cursor()
+
+# Выполняем SQL-запрос для извлечения данных
+select_query = "SELECT id_question, question FROM questions"
+cur.execute(select_query)
+
+# Получаем все строки результата
+rows = cur.fetchall()
+
+# Обрабатываем данные в Python
+processed_rows = []
+for row in rows:
+    id_question, question = row
+    processed_question = preprocess_text(question)
+    processed_rows.append((processed_question, id_question))
+
+# Выполняем SQL-запрос для загрузки обработанных данных обратно в базу данных
+update_query = "UPDATE questions SET question_processed = %s WHERE id_question = %s"
+cur.executemany(update_query, processed_rows)
+
+# Подтверждаем изменения
+conn.commit()
+
+# Закрываем курсор и соединение
+cur.close()
+conn.close()
